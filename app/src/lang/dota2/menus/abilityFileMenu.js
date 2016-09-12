20c4aa03787d91e0a773a25d92fb8f1911fa1d77
Lolleko <larry.junglas@hotmail.de>
2016-09-12 13:28:19 +0200

    Basic Dota 2 Ability support
    Added Dota2 language definitions & Menus for abilities

---
+++ app/src/lang/dota2/menus/abilityFileMenu.js
@@ -0,0 +1,11 @@
+const abilityMenu = [{
+  label: 'Add Ability',
+  click (menuItem, currentWindow) {
+    donkey.nav.newDataDialog(menuItem.target.dataset.path, 'Dota2ActiveAbility', 'Enter the name of the new ability.', null, '//DOTAAbilities//')
+  }
+}, {
+  label: 'Add Passive',
+  click (menuItem, currentWindow) {}
+}]
+
+module.exports = donkey.lang.registerMenu('Dota2AbilityFileMenu', abilityMenu)
---
+++ app/src/lang/dota2/menus/abilitySpecialEditorMenu.js
@@ -0,0 +1,19 @@
+const abilitySpecialMenu = [{
+  label: 'Add Variable',
+  click (menuItem, currentWindow) {
+    var parent = document.createElement('parent-key')
+    parent.key = '0' + menuItem.target.children.length
+    var type = document.createElement('key-value')
+    type.key = 'var_type'
+    type.value = 'FIELD_FLOAT'
+    parent.appendChild(type)
+    var variable = document.createElement('key-value')
+    variable.key = 'VARNAME'
+    variable.value = 'VARVALUE'
+    parent.appendChild(variable)
+    menuItem.target.appendChild(parent)
+    variable.focus()
+    variable.select()
+  }
+}]
+module.exports = donkey.lang.registerMenu('Dota2AbilitySpecialEditorMenu', abilitySpecialMenu)
